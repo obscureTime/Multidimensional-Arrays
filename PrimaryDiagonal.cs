e90b200dba35db4b7d4fee67a6b743478169a3ba
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2021-04-09 02:42:36 +0300

    Add files via upload

---
+++ PrimaryDiagonal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that finds the sum of matrix primary diagonal:
+
+            int[] dimensions = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[0];
+            
+            int[,] matrix = new int[rows, cols];
+
+            PrintMatrix(matrix);
+
+            int sumPrimaryDiagonal = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (row == col)
+                    {
+                        sumPrimaryDiagonal += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine(sumPrimaryDiagonal);
+
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbers = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+        }
+    }
+}
