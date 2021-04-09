31c4c14d26c6f506adfe646ab21dc879e349c3aa
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2021-04-09 15:48:17 +0300

    Add files via upload

---
+++ Pascal Triangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _7._Pascal_Triangle
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            long[][] pascalTriangle = new long[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                pascalTriangle[row] = new long[row + 1];
+                for (int col = 0; col < row + 1; col++)
+                {
+
+                    long sum = 0;
+                    if (row - 1 >= 0 && col < pascalTriangle[row - 1].Length)
+                    {
+                        sum += pascalTriangle[row - 1][col];
+                    }
+                    if (row - 1 >= 0 && col - 1 >= 0)
+                    {
+                        sum += pascalTriangle[row - 1][col - 1];
+                    }
+                    if (sum == 0)
+                    {
+                        sum = 1;
+                    }
+                    pascalTriangle[row][col] = sum;
+                }
+
+            }
+
+            for (int row = 0; row < pascalTriangle.Length; row++)
+            {
+                for (int col = 0; col < pascalTriangle[row].Length; col++)
+                {
+                    Console.Write(pascalTriangle[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
